0d4f131649c86cab88604a05b4adfe513c8daae6
Vincent Wong <vincentwwy168@gmail.com>
2023-11-24 21:06:16 +0800

    Implement is_alo

--- src/Alof.php
+++ src/Alof.php
@@ -2,10 +2,25 @@
 
 namespace Vectorial1024\AlofLib;
 
+use ArrayAccess;
+use Traversable;
+
 /**
  * The class containing various array-like object functions.
  */
 class Alof
 {
-    // todo implement some functions
+    /**
+     * Returns whether the given value is an array-like object:
+     *
+     * A value is an array-like object if it implements both `ArrayAccess` and `Traversable`.
+     * @param mixed $value the value to test
+     * @return bool
+     * @see ArrayAccess
+     * @see Traversable
+     */
+    public static function is_alo(mixed $value): bool
+    {
+        return ($value instanceof ArrayAccess) && ($value instanceof Traversable);
+    }
 }
---
+++ tests/AlofTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Vectorial1024\AlofLib\Alof;
+
+final class AlofTest extends TestCase
+{
+    public function testIsAlo()
+    {
+        // is alo
+        // implements array access and traversable
+        $this->assertTrue(Alof::is_alo(new WeakMap()));
+        // we will not test SPL Object Storage because supposedly, it is possible to be unavailable
+
+        // is not alo
+        // basically, other primitive types and objects
+        $this->assertFalse(Alof::is_alo(null));
+        $this->assertFalse(Alof::is_alo(false));
+        $this->assertFalse(Alof::is_alo(true));
+        $this->assertFalse(Alof::is_alo(10));
+        $this->assertFalse(Alof::is_alo(12.5));
+        $this->assertFalse(Alof::is_alo('hello world'));
+        $this->assertFalse(Alof::is_alo([]));
+        $this->assertFalse(Alof::is_alo(new stdClass()));
+        $handle = fopen(__FILE__, 'r');
+        $this->assertFalse(Alof::is_alo($handle));
+    }
+}
